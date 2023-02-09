36637fbe6fd551b51bfc5f05c6373da26b03d192
Yunus <yunusinne@gmail.com>
2023-02-09 10:51:44 +0300

    queue  implementation

---
+++ Queue/Queue.js
@@ -0,0 +1,35 @@
+export default class Queue {
+    constructor(){
+        this.items = [];
+    }
+
+    // insert at end of the queue
+    enqueue(element){
+        this.items.push(element);
+    }
+
+    // removed from the beginning of the queue. 
+    dequeue(){
+       return this.items.shift();
+    }
+
+    isEmpty(){
+        return this.items.length ===0;
+    }
+
+    peek(){
+        if(!this.isEmpty()){
+            return this.items[0];
+        }
+        return null;
+    }
+
+    size(){
+        return this.items.length;
+    }
+
+    print(){
+        console.log(this.items.toString());
+    }
+
+}
